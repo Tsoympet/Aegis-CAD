60769753c74d224edee487e3067d7d747e961465
Tsoympet <tsoympet7@gmail.com>
2025-11-16 12:42:08 +0200

    Update GltfExporter.cpp

--- src/cad/GltfExporter.cpp
+++ src/cad/GltfExporter.cpp
@@ -1,8 +1,21 @@
 #include "GltfExporter.h"
+#include <RWGltf_CafWriter.hxx>
+#include <XCAFDoc_DocumentTool.hxx>
+#include <TDocStd_Document.hxx>
+#include <XCAFApp_Application.hxx>
+#include <STEPCAFControl_Writer.hxx>
+#include <BinXCAFDrivers.hxx>
 
 bool GltfExporter::exportGltf(const std::string& path, const TopoDS_Shape& shape)
 {
-    (void)path;
-    (void)shape;
-    return false;
+    Handle(TDocStd_Document) doc;
+    Handle(XCAFApp_Application) app = XCAFApp_Application::GetApplication();
+    app->NewDocument("MDTV-XCAF", doc);
+
+    Handle(XCAFDoc_ShapeTool) shapeTool = XCAFDoc_DocumentTool::ShapeTool(doc->Main());
+    shapeTool->AddShape(shape);
+
+    RWGltf_CafWriter writer(path.c_str(), false);
+    writer.Perform(doc, path.c_str());
+    return true;
 }
