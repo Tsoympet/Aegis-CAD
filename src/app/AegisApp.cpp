ebd8e4293d6e162c4f7890f613b9bc7eee5b3dde
Tsoympet <tsoympet7@gmail.com>
2025-11-16 11:55:34 +0200

    Implement translation loading in AegisApp
    Added translation loading functionality to AegisApp.

--- src/app/AegisApp.cpp
+++ src/app/AegisApp.cpp
@@ -1,8 +1,21 @@
 #include "AegisApp.h"
+#include <QLocale>
+#include <QDebug>
 
 AegisApp::AegisApp(int& argc, char** argv)
     : QApplication(argc, argv)
 {
     setApplicationName("AegisCAD");
     setOrganizationName("AegisCAD Project");
+    setApplicationVersion("1.0.0");
+    loadTranslations();
+}
+
+void AegisApp::loadTranslations()
+{
+    const QString locale = QLocale::system().name();
+    if (m_translator.load("aegiscad_" + locale, ":/translations/")) {
+        installTranslator(&m_translator);
+        qInfo() << "Loaded translation for" << locale;
+    }
 }
