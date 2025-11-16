ca49c0804e79dbe7954664c8dc4880ba26c9ebd1
Tsoympet <tsoympet7@gmail.com>
2025-11-16 11:55:14 +0200

    Update AegisApp.h

--- src/app/AegisApp.h
+++ src/app/AegisApp.h
@@ -1,8 +1,13 @@
 #pragma once
 #include <QApplication>
+#include <QTranslator>
 
 class AegisApp : public QApplication
 {
 public:
     AegisApp(int& argc, char** argv);
+
+private:
+    void loadTranslations();
+    QTranslator m_translator;
 };
