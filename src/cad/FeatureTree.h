c8c939a71a119382c966e6a7fb0f3beb8869b79d
Tsoympet <tsoympet7@gmail.com>
2025-11-16 12:37:56 +0200

    Add new method addFeature to FeatureTree class

--- src/cad/FeatureTree.h
+++ src/cad/FeatureTree.h
@@ -13,6 +13,7 @@ class FeatureTree
 public:
     int addFeature(const std::string& name, int parentId = 0);
     const std::vector<FeatureEntry>& entries() const { return m_entries; }
+
 private:
     std::vector<FeatureEntry> m_entries;
     int m_nextId {1};
