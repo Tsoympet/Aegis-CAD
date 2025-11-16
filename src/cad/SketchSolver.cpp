66c1c757466ca66319922796fe602dcfea0a9daa
Tsoympet <tsoympet7@gmail.com>
2025-11-16 12:39:52 +0200

    Update TODO comment for constraint solving algorithm

--- src/cad/SketchSolver.cpp
+++ src/cad/SketchSolver.cpp
@@ -5,6 +5,6 @@ bool SketchSolver::solve(SketchDocument& doc, int iterations, double alpha)
     (void)doc;
     (void)iterations;
     (void)alpha;
-    // TODO: implement actual constraint solving algorithm
+    // TODO: implement geometric constraint solver
     return true;
 }
