c452387a1e39831ac17128c4d3e901dea49ccfbb
Tsoympet <tsoympet7@gmail.com>
2025-11-16 11:57:20 +0200

    Add new slots and QAction members to MainWindow

--- src/app/MainWindow.h
+++ src/app/MainWindow.h
@@ -6,18 +6,39 @@ class AegisAssistantDock;
 class PythonConsoleDock;
 class ReverseEngineerDock;
 
+class QAction;
+class QLabel;
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 public:
     explicit MainWindow(QWidget* parent = nullptr);
     ~MainWindow() override;
+
+private slots:
+    void newSketch();
+    void importSTEP();
+    void exportGLTF();
+    void runAnalysis();
+    void showAIDock();
+
 private:
     void createMenus();
+    void createToolBar();
     void createDocks();
+    void createStatusBar();
 
     OccView*             m_view = nullptr;
     AegisAssistantDock*  m_aiDock = nullptr;
     PythonConsoleDock*   m_pyDock = nullptr;
     ReverseEngineerDock* m_revDock = nullptr;
+
+    QAction* m_actionNew = nullptr;
+    QAction* m_actionImport = nullptr;
+    QAction* m_actionExport = nullptr;
+    QAction* m_actionAnalyze = nullptr;
+    QAction* m_actionAI = nullptr;
+
+    QLabel*  m_statusLabel = nullptr;
 };
