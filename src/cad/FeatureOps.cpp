6bd64ad67437dbf17713832d7efb93684bb761fa
Tsoympet <tsoympet7@gmail.com>
2025-11-16 12:31:26 +0200

    Implement shape creation functions in FeatureOps
    Added functions to create box, cylinder, and sphere shapes.

--- src/cad/FeatureOps.cpp
+++ src/cad/FeatureOps.cpp
@@ -1,7 +1,26 @@
 #include "FeatureOps.h"
+#include <BRepPrimAPI_MakeBox.hxx>
+#include <BRepPrimAPI_MakeCylinder.hxx>
+#include <BRepPrimAPI_MakeSphere.hxx>
 
 std::shared_ptr<TopoDS_Shape> FeatureOps::build(const std::vector<FeatureNode>& tree)
 {
     (void)tree;
+    // TODO: Implement full feature replay (extrude, cut, fillet etc.)
     return nullptr;
+}
+
+TopoDS_Shape FeatureOps::makeBox(double dx, double dy, double dz)
+{
+    return BRepPrimAPI_MakeBox(dx, dy, dz).Shape();
+}
+
+TopoDS_Shape FeatureOps::makeCylinder(double r, double h)
+{
+    return BRepPrimAPI_MakeCylinder(r, h).Shape();
+}
+
+TopoDS_Shape FeatureOps::makeSphere(double r)
+{
+    return BRepPrimAPI_MakeSphere(r).Shape();
 }
