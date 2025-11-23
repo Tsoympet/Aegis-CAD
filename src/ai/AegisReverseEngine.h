3f95cf92f3b783635e76b829de5e66e7fb03cc6a
Tsoympet <tsoympet7@gmail.com>
2025-11-23 13:08:25 +0200

    Refactor AegisReverseEngine class and structures

--- src/ai/AegisReverseEngine.h
+++ src/ai/AegisReverseEngine.h
@@ -1,19 +1,32 @@
 #pragma once
+#include <QObject>
 #include <QString>
+#include <memory>
+#include <vector>
+#include <opencv2/core.hpp>
 
-struct ReverseSpec {
-    QString objectName;
-    QString domain;
-    QString textInfo;
-    int     imageCount {0};
-};
+class TopoDS_Shape;
 
-struct ReverseResult {
-    QString summary;
-};
-
-class AegisReverseEngine
+class AegisReverseEngine : public QObject
 {
+    Q_OBJECT
 public:
-    ReverseResult runReverse(const ReverseSpec& spec);
+    explicit AegisReverseEngine(QObject* parent = nullptr);
+    ~AegisReverseEngine() override;
+
+    bool initializePython();
+    QString analyzeImage(const QString& imagePath);
+    QString analyzeText(const QString& text);
+    std::shared_ptr<TopoDS_Shape> reconstructModel();
+    QString summary() const;
+
+signals:
+    void logMessage(const QString& msg);
+    void modelReady(const std::shared_ptr<TopoDS_Shape>& shape);
+
+private:
+    bool m_pythonInitialized{false};
+    cv::Mat m_lastImage;
+    QString m_lastText;
+    QString runPythonFusion(const QString& prompt, const cv::Mat& image);
 };
