5d361d07f8a052a2a8d32c8d6b5d1697fe58ce34
Tsoympet <tsoympet7@gmail.com>
2025-11-23 13:11:36 +0200

    Update AegisAssistantDock.h

--- src/ui/AegisAssistantDock.h
+++ src/ui/AegisAssistantDock.h
@@ -1,29 +1,37 @@
 #pragma once
 #include <QDockWidget>
-#include <functional>
-#include "ai/AegisAIEngine.h"
+#include <QTextEdit>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QLabel>
+#include <memory>
 
-class QTextEdit;
-class QLineEdit;
-class QPushButton;
+class AegisAIEngine;
+class AegisReverseEngine;
 
 class AegisAssistantDock : public QDockWidget
 {
     Q_OBJECT
 public:
     explicit AegisAssistantDock(QWidget* parent = nullptr);
+    ~AegisAssistantDock() override;
 
-    void setEngine(AegisAIEngine* engine);
-    void setContextProvider(std::function<AegisContext()> provider);
+signals:
+    void sendCommand(const QString& cmd);
 
 private slots:
-    void onSend();
+    void onSendClicked();
+    void handleAIResponse(const QString& msg);
+    void handleLog(const QString& msg);
 
 private:
-    AegisAIEngine* m_engine = nullptr;
-    std::function<AegisContext()> m_ctxProvider;
+    std::unique_ptr<AegisAIEngine> m_ai;
+    std::unique_ptr<AegisReverseEngine> m_rev;
 
-    QTextEdit*  m_chat = nullptr;
-    QLineEdit*  m_input = nullptr;
-    QPushButton* m_send = nullptr;
+    QTextEdit* m_console{nullptr};
+    QLineEdit* m_input{nullptr};
+    QPushButton* m_sendBtn{nullptr};
+
+    void connectSignals();
 };
