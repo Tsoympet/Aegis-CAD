75c24d39688cf7b01026a122b52c112fbf9fb290
Tsoympet <tsoympet7@gmail.com>
2025-11-23 17:47:50 +0200

    Update ProjectIO.h

--- src/app/ProjectIO.h
+++ src/app/ProjectIO.h
@@ -1,9 +1,26 @@
 #pragma once
 #include <QString>
+#include <QJsonObject>
+#include <QVector>
+#include <memory>
+
+struct PartContext;
 
 class ProjectIO
 {
 public:
-    static bool save(const QString& path);
-    static bool load(const QString& path);
+    static bool saveProject(const QString& filePath,
+                            const QJsonObject& cadState,
+                            const QJsonObject& analysisState,
+                            const PartContext& aiContext,
+                            const QVector<QString>& aiConsoleLog);
+
+    static bool loadProject(const QString& filePath,
+                            QJsonObject& cadState,
+                            QJsonObject& analysisState,
+                            PartContext& aiContext,
+                            QVector<QString>& aiConsoleLog);
+
+    static QJsonObject readJsonFile(const QString& path);
+    static bool writeJsonFile(const QString& path, const QJsonObject& obj);
 };
