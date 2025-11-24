442ccdbe9bc51b8c5b3be8885c527397b96aba97
Tsoympet <tsoympet7@gmail.com>
2025-11-24 17:34:27 +0200

    Update ProjectIO.h

--- src/app/ProjectIO.h
+++ src/app/ProjectIO.h
@@ -2,6 +2,7 @@
 #include <QString>
 #include <QJsonObject>
 #include <QVector>
+#include <QImage>
 #include <memory>
 
 struct PartContext;
@@ -13,13 +14,16 @@ public:
                             const QJsonObject& cadState,
                             const QJsonObject& analysisState,
                             const PartContext& aiContext,
-                            const QVector<QString>& aiConsoleLog);
+                            const QVector<QString>& aiConsoleLog,
+                            const QImage& thumbnail,
+                            const QString& thumbFileName = "project_thumbnail.png");
 
     static bool loadProject(const QString& filePath,
                             QJsonObject& cadState,
                             QJsonObject& analysisState,
                             PartContext& aiContext,
-                            QVector<QString>& aiConsoleLog);
+                            QVector<QString>& aiConsoleLog,
+                            QString& thumbPath);
 
     static QJsonObject readJsonFile(const QString& path);
     static bool writeJsonFile(const QString& path, const QJsonObject& obj);
