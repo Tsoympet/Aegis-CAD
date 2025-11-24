1f9b714476df108e4d89d89ca9530faf5560bc70
Tsoympet <tsoympet7@gmail.com>
2025-11-24 17:41:57 +0200

    Update AegisApp.cpp

--- src/app/AegisApp.cpp
+++ src/app/AegisApp.cpp
@@ -1,22 +1,59 @@
-#pragma once
-#include <QApplication>
-#include <QString>
-#include <memory>
+#include "AegisApp.h"
+#include "MainWindow.h"
+#include <QStyleFactory>
+#include <QSplashScreen>
+#include <QTimer>
+#include <QPixmap>
+#include <QDir>
+#include <QDebug>
 
-class MainWindow;
+// Optional OpenCascade includes
+#include <Standard_Version.hxx>
+#include <OSD_Environment.hxx>
 
-class AegisApp
+AegisApp::AegisApp(int& argc, char** argv)
+    : m_app(argc, argv)
 {
-public:
-    AegisApp(int& argc, char** argv);
-    ~AegisApp();
+    m_app.setApplicationName("AegisCAD");
+    m_app.setApplicationVersion("1.0");
+    m_app.setOrganizationName("Aegis Dynamics");
 
-    int run();
+    initStyle();
+    initOpenCascade();
+}
 
-private:
-    QApplication m_app;
-    std::unique_ptr<MainWindow> m_mainWindow;
+AegisApp::~AegisApp() = default;
 
-    void initStyle();
-    void initOpenCascade();
-};
+void AegisApp::initStyle()
+{
+    m_app.setStyle(QStyleFactory::create("Fusion"));
+    QPalette darkPalette;
+    darkPalette.setColor(QPalette::Window, QColor(45, 45, 48));
+    darkPalette.setColor(QPalette::WindowText, Qt::white);
+    darkPalette.setColor(QPalette::Base, QColor(30, 30, 30));
+    darkPalette.setColor(QPalette::Text, Qt::white);
+    m_app.setPalette(darkPalette);
+}
+
+void AegisApp::initOpenCascade()
+{
+    qInfo() << "Initializing OpenCascade version:" << OCC_VERSION_COMPLETE;
+    OSD_Environment env;
+    env.Set("CSF_LANGUAGE", "ENGLISH");
+}
+
+int AegisApp::run()
+{
+    QPixmap splashImg(":/icons/app_icon.png");
+    QSplashScreen splash(splashImg);
+    splash.showMessage("Initializing AegisCAD...", Qt::AlignBottom | Qt::AlignHCenter, Qt::white);
+    splash.show();
+    m_app.processEvents();
+
+    m_mainWindow = std::make_unique<MainWindow>();
+    m_mainWindow->show();
+
+    QTimer::singleShot(1500, &splash, &QSplashScreen::close);
+
+    return m_app.exec();
+}
