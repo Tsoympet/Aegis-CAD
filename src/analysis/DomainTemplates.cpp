d277cc361bc064b5b3dc6b0138dec010d4434569
Tsoympet <tsoympet7@gmail.com>
2025-11-27 00:26:36 +0200

    Update DomainTemplates.cpp

--- src/analysis/DomainTemplates.cpp
+++ src/analysis/DomainTemplates.cpp
@@ -1,1 +1,12 @@
 #include "DomainTemplates.h"
+
+QVector<DomainTemplate> DomainTemplates::defaults()
+{
+    return {
+        { "Car Chassis", "Static frame with suspension load points", 7850, 2.1e11, 0.3 },
+        { "Ship Deck", "Marine deck structure with distributed load", 7800, 2.0e11, 0.28 },
+        { "Aircraft Wing", "Aluminum wing panel under bending", 2700, 7.0e10, 0.33 },
+        { "Armor Hull", "Composite steel armor section", 7850, 2.05e11, 0.29 },
+        { "Generic Steel", "Default isotropic steel template", 7850, 2.1e11, 0.3 }
+    };
+}
