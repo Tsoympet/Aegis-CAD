f9cbdf6e87967d9a41b87856e9990f49e09409b7
Tsoympet <tsoympet7@gmail.com>
2025-11-27 17:21:38 +0200

    Update PythonConsoleDock.h

--- src/ui/PythonConsoleDock.h
+++ src/ui/PythonConsoleDock.h
@@ -1,21 +1,37 @@
 #pragma once
 #include <QDockWidget>
+#include <QTextEdit>
+#include <QLineEdit>
+#include <QVBoxLayout>
+#include <QKeyEvent>
+#include <QScrollBar>
+#include <QStringList>
+#include <QProcessEnvironment>
 
-class QTextEdit;
-class QPlainTextEdit;
-class QPushButton;
-
+/// Dockable Python interactive console for embedded scripting.
 class PythonConsoleDock : public QDockWidget
 {
     Q_OBJECT
 public:
     explicit PythonConsoleDock(QWidget* parent = nullptr);
+    ~PythonConsoleDock() override;
+
+    void printMessage(const QString& msg, const QColor& color = Qt::lightGray);
+    void executeCommand(const QString& command);
+
+protected:
+    void keyPressEvent(QKeyEvent* e) override;
 
 private slots:
-    void onRunClicked();
+    void onCommandEntered();
 
 private:
-    QTextEdit*      m_output = nullptr;
-    QPlainTextEdit* m_input  = nullptr;
-    QPushButton*    m_runBtn = nullptr;
+    void initPython();
+    void finalizePython();
+
+    QTextEdit*  output = nullptr;
+    QLineEdit*  input  = nullptr;
+    QStringList history;
+    int         historyIndex = -1;
+    bool        initialized = false;
 };
