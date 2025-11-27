d53165207f4c5d71172964de4803d519e2af9a18
Tsoympet <tsoympet7@gmail.com>
2025-11-27 20:13:06 +0200

    Update AssemblyDocument.cpp

--- src/assembly/AssemblyDocument.cpp
+++ src/assembly/AssemblyDocument.cpp
@@ -1,11 +1,51 @@
 #include "AssemblyDocument.h"
+#include <QDebug>
 
-int AssemblyDocument::addNode(const std::string& partId, int parentId)
+AssemblyDocument::AssemblyDocument() {}
+
+AssemblyNode* AssemblyDocument::addPart(const QString& name, const QString& path)
 {
-    AssemblyNodeRef r;
-    r.id = m_nextId++;
-    r.parentId = parentId;
-    r.partId = partId;
-    m_nodes.push_back(r);
-    return r.id;
+    auto node = std::make_shared<AssemblyNode>(name, path);
+    m_parts.append(node);
+    qDebug() << "Added part:" << name << "from" << path;
+    return node.get();
+}
+
+bool AssemblyDocument::removePart(const QString& name)
+{
+    for (int i = 0; i < m_parts.size(); ++i) {
+        if (m_parts[i]->name() == name) {
+            m_parts.remove(i);
+            return true;
+        }
+    }
+    return false;
+}
+
+void AssemblyDocument::addJoint(const Joint& j)
+{
+    m_joints.append(j);
+    qDebug() << "Joint added:" << j.name;
+}
+
+void AssemblyDocument::removeJoint(const QString& name)
+{
+    for (int i = 0; i < m_joints.size(); ++i) {
+        if (m_joints[i].name == name) {
+            m_joints.remove(i);
+            break;
+        }
+    }
+}
+
+void AssemblyDocument::solveConstraints()
+{
+    m_solver.solve(m_joints, m_parts);
+    m_graph.update(m_parts);
+}
+
+void AssemblyDocument::clear()
+{
+    m_parts.clear();
+    m_joints.clear();
 }
