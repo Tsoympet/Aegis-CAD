357f6fb3425f2e8d2fdae3e73478a29e7a4a810a
Tsoympet <tsoympet7@gmail.com>
2025-11-27 20:18:46 +0200

    Update TransformGraph.cpp

--- src/assembly/TransformGraph.cpp
+++ src/assembly/TransformGraph.cpp
@@ -1,1 +1,7 @@
 #include "TransformGraph.h"
+#include <QDebug>
+
+void TransformGraph::update(const QVector<std::shared_ptr<AssemblyNode>>& parts)
+{
+    qDebug() << "TransformGraph updated for" << parts.size() << "nodes.";
+}
