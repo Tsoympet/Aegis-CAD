bc5a1f7e136e2882ff3bda4f93c1557461ce9c6b
Tsoympet <tsoympet7@gmail.com>
2025-11-27 20:17:30 +0200

    Update Mates.h

--- src/assembly/Mates.h
+++ src/assembly/Mates.h
@@ -1,6 +1,15 @@
 #pragma once
+#include <QString>
+#include <gp_Pnt.hxx>
+#include <gp_Dir.hxx>
 
 struct Mate
 {
-    int id {0};
+    QString name;
+    QString partA;
+    QString partB;
+    gp_Pnt anchorA;
+    gp_Pnt anchorB;
+    gp_Dir normalA;
+    gp_Dir normalB;
 };
