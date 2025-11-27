2275cb58b065caf1254928d6ea9e63bd726cf690
Tsoympet <tsoympet7@gmail.com>
2025-11-27 20:13:34 +0200

    Update AssemblyNode.h

--- src/assembly/AssemblyNode.h
+++ src/assembly/AssemblyNode.h
@@ -1,9 +1,26 @@
 #pragma once
-#include <string>
+#include <QString>
+#include <gp_Trsf.hxx>
+#include <TopoDS_Shape.hxx>
 
-struct AssemblyNode
+/// Represents an individual part node within an assembly.
+class AssemblyNode
 {
-    int         id {0};
-    int         parentId {0};
-    std::string partId;
+public:
+    AssemblyNode(const QString& name, const QString& path);
+
+    const QString& name() const { return m_name; }
+    const QString& path() const { return m_path; }
+
+    gp_Trsf& transform() { return m_trsf; }
+    const gp_Trsf& transform() const { return m_trsf; }
+
+    const TopoDS_Shape& shape() const { return m_shape; }
+    void setShape(const TopoDS_Shape& s) { m_shape = s; }
+
+private:
+    QString m_name;
+    QString m_path;
+    gp_Trsf m_trsf;
+    TopoDS_Shape m_shape;
 };
